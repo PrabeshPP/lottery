d688b63a3dcd3a00595531d2c2c3453ef42bd698
Prabesh <prabesh.bista369@gmail.com>
2022-02-02 13:55:27 +0545

    lottery contract

---
+++ contract/lottery.sol
@@ -0,0 +1,33 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract Lottery{
+    address payable manager;
+    address[] players;
+
+    constructor(){
+        manager=payable(msg.sender);
+    }
+
+    function enter()public payable {
+        require(msg.value>0.1 ether ,"You have to pay more that 0.1 ether to participate in the lottery game");
+        players.push(msg.sender);
+    }
+
+    function randomGenerator()private view returns(uint){
+        uint value=uint(keccak256(abi.encodePacked(block.difficulty,block.timestamp,players)));
+
+        return value;
+
+    }
+
+    function PickWinner()external payable{
+        uint randomValue=randomGenerator() % players.length;
+        payable(players[randomValue]).transfer(address(this).balance);
+        players=new address[](0);
+    }
+
+    function getPlayers()public view returns(address[] memory){
+        return players;
+    }
+}
