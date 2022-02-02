7699397391dfce7189b16e7ce393e0aa9f0b5ddc
Prabesh <prabesh.bista369@gmail.com>
2022-02-02 14:09:38 +0545

    .

--- compile.js
+++ compile.js
@@ -1,25 +1,4 @@
-let path=require('path');
-let fs=require('fs');
-let solc=require('solc');
+let path=require("path");
+let fs=require("fs");
+let solc=require("solc");
 let lotteryPath=path.resolve(__dirname,"contract","lottery.sol");
-let source=fs.readFileSync(lotteryPath,"utf-8");
-
-// 
-
-var input={
-    language: 'Solidity',
-    sources:{
-        'lottery.sol':{
-            content:source
-        }
-    },
-    settings:{
-        outputSelection: {
-            "*":{
-                "*":['*']
-            }
-        }
-    }
-};
-
-
